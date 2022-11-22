5f9a84bbab5a55411e5632a36c2664aca02c2bee
magabaldonVD <109799438+magabaldonVD@users.noreply.github.com>
2022-11-22 21:32:07 +0100

    Add message text model

---
+++ WhatsApp/Messages/MessageText.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhatsApp.Messages.Object;
+
+namespace WhatsApp.Messages
+{
+    public class MessageText : MessageBase
+    {
+        public MessageText(string to, string text, bool previewUrl) : base("text", to) 
+        {
+            Text = new Text(text, previewUrl);
+        }
+
+        [JsonProperty("text")]
+        public Text Text;
+    }
+}
